36a5d4199359e9b29743670caa2bbcd6ac2ac6ef
Aditya Shinde <adishinde611@gmail.com>
2024-03-03 21:53:27 +0530

    Distinct     Order BY     Limit     Like

--- session_6.sql
+++ session_6.sql
@@ -53,3 +53,22 @@ if order by is depending on two columns then if their is clash between two rows 
 */
 SELECT * FROM student;
 SELECT * from student ORDER BY year_of_exp DESC LIMIT 2; -- display first 2 rows
+
+/* LIKE */
+
+SHOW TABLES;
+
+DESCRIBE courses;
+
+DESC student;
+/* non cas sensitive */
+select student_id,student_fname FROM student WHERE student_fname LIKE '%a%'; -- Like is used to find with given initial in string %a% means if a contain in student_fname at any position display
+
+select student_id,student_fname FROM student WHERE student_fname LIKE 'a%'; -- Like is used to find with given initial in string a% means if a contain as first character in student_fname  display
+
+select student_id,student_fname FROM student WHERE student_fname LIKE '%a'; -- Like is used to find with given initial in string %a% means if a contain at last position in student_fname display
+
+SELECT * FROM student;
+
+SELECT student_fname,student_id FROM student WHERE student_fname LIKE '_____'; -- it count the underscore if 5 then print 5 letter name 
+
